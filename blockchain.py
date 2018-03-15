27d362af73ba3f2d8f8ddfcdc412e357b3a23ba9
Adeen Shukla <adeens357@gmail.com>
2018-03-15 12:24:06 +0530

    Blockchain class blueprint

---
+++ blockchain.py
@@ -0,0 +1,18 @@
+class Blockchain(object):
+    def __init__(self):
+        self.chain = []
+        self.current_transactions = []
+
+    def new_block(self):
+        pass
+
+    def new_transaction(self):
+        pass
+
+    @staticmethod
+    def hash(block):
+        pass
+
+    @property
+    def last_block(self):
+        pass
