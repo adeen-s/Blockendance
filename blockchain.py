7af1d5129d10646a88713448838bba404b4b17fd
Adeen Shukla <adeens357@gmail.com>
2018-03-15 12:39:11 +0530

    Genesis block code and new block implementation

--- blockchain.py
+++ blockchain.py
@@ -1,10 +1,28 @@
+import hashlib
+import json
+from time import time
+
 class Blockchain(object):
     def __init__(self):
         self.chain = []
         self.current_transactions = []
 
+        # For genesis block
+        self.new_block(previous_hash = 1, proof = 100)
+
     def new_block(self):
-        pass
+        block = {
+            "index": len(self.chain) + 1,
+            "timestamp": time(),
+            "transactions": self.current_transactions,
+            "proof": proof,
+            "previous_hash": previous_hash or self.hash(self.chain[-1])
+        }
+
+        self.current_transaction = []
+
+        self.chain.append(block)
+        return block
 
     def new_transaction(self, sender, recipient, amount):
         self.current_transactions.append({
