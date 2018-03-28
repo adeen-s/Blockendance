14ddac2096d0ed59b6e8b78488ff0342b1bb3b21
Adeen Shukla <adeens357@gmail.com>
2018-03-28 19:19:51 +0530

    Add ability to input data and add to blockchain

--- blockchain.py
+++ blockchain.py
@@ -8,48 +8,44 @@ from block import *
 
 app = Flask(__name__)
 
-def checkIntegrity(chain):
-    for i, block in enumerate(chain):
-        if i < len(chain) - 1:
-            print("Checking integrity of block {}".format(i))
-            if block.hash_block() != chain[i+1].prev_hash:
-                print("Chain has been modified at block index {}".format(i))
-                break
-        else:
-            print("Chain has not been modified")
-
 blockchain = create_genesis_block()
 previous_block = blockchain[0]
 
-# blocks after the genesis block
-num_of_blocks = 20
-
-# Add blocks to the chain
-for i in range(0, num_of_blocks):
-    block_to_add = next_block(previous_block)
+def add_block(form):
+    data = []
+    i = 1
+    global previous_block
+    while form.get("roll_no{}".format(i)):
+        data.append(form.get("roll_no{}".format(i)))
+        i += 1
+    block_to_add = next_block(previous_block, data)
     blockchain.append(block_to_add)
     previous_block = block_to_add
     print ("Block #{} has been added to the blockchain!".format(block_to_add.index))
     print ("Hash: {}\n".format(block_to_add.hash))
+    print(data)
+    return "Block #{} has been added to the blockchain!".format(block_to_add.index)
 
-for i, block in enumerate(blockchain):
-    if(i == 15):
-        block.data = "Hello There, Data changed"
-    print(block.data)
+@app.route('/',  methods = ['GET'])
+def index():
+    return render_template("index.html")
 
-checkIntegrity(blockchain)
-
-@app.route('/', methods=['GET'])
-def hello():
-    return render_template("index.html", name="Adeen", date=dt.date.today())
-
-@app.route('/', methods=['POST'])
+@app.route('/', methods = ['POST'])
 def parse_request():
-    if(request.form.get("number")):
+    if(request.form.get("name")):
+        return render_template("class.html",
+                                name = request.form.get("name"),
+                                date = dt.date.today())
+    elif(request.form.get("number")):
         return render_template("attendance.html",
-                                course=request.form.get("course"),
-                                year=request.form.get("year"),
-                                number=int(request.form.get("number")))
-    elif(request.form.get("roll_no")):
-        return
-    return "Invalid POST request. This incident has been recorded."
+                                course = request.form.get("course"),
+                                year = request.form.get("year"),
+                                number = int(request.form.get("number")))
+    elif(request.form.get("roll_no1")):
+        return add_block(request.form)
+
+    else:
+        return "Invalid POST request. This incident has been recorded."
+
+if __name__ == "__main__":
+    main()
--- newBlock.py
+++ newBlock.py
@@ -1,9 +1,9 @@
 from block import *
 import datetime as dt
 
-def next_block(last_block):
+def next_block(last_block, data):
     this_index = last_block.index + 1
     this_timestamp = dt.datetime.now()
-    this_data = "Hey Adeen! I'm block " + str(this_index)
-    this_hash = last_block.hash
-    return Block(this_index, this_timestamp, this_data, this_hash)
+    this_data = data
+    this_prev_hash = last_block.hash
+    return Block(this_index, this_timestamp, this_data, this_prev_hash)
